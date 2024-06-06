da16bddde1d7bc25a2e7444ebd3c0261af960dbf
Imesh <imeshthanapathi@gmail.com>
2024-06-06 18:28:20 +0530

    Content box added

---
+++ app/components/contentBox/contentBox.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './contentBox.css'
+
+function ContentBox() {
+  return (
+    <div className='content-box'>
+        <img className='cross-icon' src='../../assets/crossIcon.jpg' alt='icon'></img>
+        <h1>Welcome back, John Doe</h1>
+        <p>The end of the year is coming. Are you planning your performance interviews? You can do this super efficiently with Acmy.</p>
+        <a href=''>Look here for more information</a>
+    </div>
+  )
+}
+
+export default ContentBox
--- app/components/dashboard/dashboard.tsx
+++ app/components/dashboard/dashboard.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './dashboard.css'
 import Navbar from '../navbar/navbar'
+import ContentBox from '../contentBox/contentBox'
 
 function Dashboard() {
   return (
     <div className='dashboard col-10'>
         <Navbar />
+        <ContentBox />
     </div>
   )
 }
